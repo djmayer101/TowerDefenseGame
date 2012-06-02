bb9cfb51e2e567c45902efe4fdbf579af2e9fcd4
jvarley <jvarley@mit.edu>
2012-06-01 20:16:06 -0700

    new gameMain.java

--- src/com/game/main/GameMain.java
+++ src/com/game/main/GameMain.java
@@ -2,16 +2,28 @@ package com.game.main;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 
 public class GameMain extends Activity {
     /** Called when the activity is first created. */
+	Button myButton;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Button myButton = (Button) findViewById(R.id.startbtn);
+        myButton = (Button) findViewById(R.id.startbtn);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
         
     }
+    
+    public void startGame(View view){
+    	myButton.setText("button pushed");
+    }
+    
 }
