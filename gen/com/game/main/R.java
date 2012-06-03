d1c3267cd23235e15772e4cea9f642819ca305a6
David Mayer <djmayer101@gmail.com>
2012-06-03 11:35:31 -0700

    added images w\ text to menu

--- gen/com/game/main/R.java
+++ gen/com/game/main/R.java
@@ -11,12 +11,17 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int greenstar=0x7f020000;
-        public static final int gs=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int redstar=0x7f020003;
-        public static final int yellowsquare=0x7f020004;
-        public static final int yellowstar=0x7f020005;
+        public static final int awesome_castle=0x7f020000;
+        public static final int awesome_castle_desc=0x7f020001;
+        public static final int basic_castle=0x7f020002;
+        public static final int basic_castle_desc=0x7f020003;
+        public static final int greenstar=0x7f020004;
+        public static final int gs=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int redstar=0x7f020007;
+        public static final int start_image=0x7f020008;
+        public static final int yellowsquare=0x7f020009;
+        public static final int yellowstar=0x7f02000a;
     }
     public static final class id {
         public static final int basic_tower=0x7f060002;
