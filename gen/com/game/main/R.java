0af61aa69f10ea4cbae24c91be2e2f8794f8781a
jvarley <jvarley@mit.edu>
2012-06-03 12:02:43 -0700

    working TableLayout

--- gen/com/game/main/R.java
+++ gen/com/game/main/R.java
@@ -11,18 +11,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int greenstar=0x7f020000;
-        public static final int gs=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int redstar=0x7f020003;
-        public static final int yellowsquare=0x7f020004;
-        public static final int yellowstar=0x7f020005;
+        public static final int clearyellow=0x7f020000;
+        public static final int greenstar=0x7f020001;
+        public static final int gs=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int redstar=0x7f020004;
+        public static final int yellowsquare=0x7f020005;
+        public static final int yellowstar=0x7f020006;
     }
     public static final class id {
         public static final int basic_tower=0x7f060002;
+        public static final int gametablelayout=0x7f060006;
         public static final int good_tower=0x7f060003;
         public static final int new_game=0x7f060000;
-        public static final int placing_tower=0x7f060006;
+        public static final int placing_tower=0x7f060007;
         public static final int start_round=0x7f060004;
         public static final int startbtn=0x7f060001;
         public static final int tv=0x7f060005;
