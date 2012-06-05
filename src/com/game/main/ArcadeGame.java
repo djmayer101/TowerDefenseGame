30035947dd15438bc2ed02c92eb273736f52aaa5
David Mayer <djmayer101@gmail.com>
2012-06-04 18:33:49 -0700

    added ArcadeGame class

--- src/com/game/main/ArcadeGame.java
+++ src/com/game/main/ArcadeGame.java
@@ -1,14 +1,98 @@
 package com.game.main;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
 public abstract class ArcadeGame extends LinearLayout {
 
+	private Context mContex;
+	
+	private Timer mUpdateTimer;
+	
+	private long mPeriod = 1000;
+	
+	
+	
+	
+	public ArcadeGame(Context context) {
+		super(context);
+		mContex = context;
+	}
+	
 	public ArcadeGame(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
+		mContex = context;
 	}
+	
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		super.onLayout(changed, l, t, r, b);
+		try {
+			initialize();
+			startUpdateTimer();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setUpdatePeriod(long period) {
+		mPeriod = period;
+	}
+	
+	
+	
+
+	protected void startUpdateTimer() {
+		mUpdateTimer = new Timer();
+		mUpdateTimer.schedule(new UpdateTask(), 0, mPeriod);
+	
+	}
+	
+	protected void stopUpdateTimer() {
+		if (mUpdateTimer != null)
+			mUpdateTimer.cancel();
+	}
+	
+	public Context getContex() {
+		return mContex;
+	}
+	
+	protected Bitmap getImage(int id) {
+		return BitmapFactory.decodeResource(mContex.getResources(), id);
+	}
+	
+	abstract protected void updatePhysics();
+	
+	abstract protected void initialize();
+	
+	abstract protected boolean gameOver();
+	
+	abstract protected long getScore();
+	
+	private class UpdateTask extends TimerTask {
+		@Override
+		public void run() {
+			updatePhysics();
+			
+			postInvalidate();
+		}
+	}
+	
+	public void halt() {
+		stopUpdateTimer();
+	}
+	
+	public void resume() {
+		initialize();
+		startUpdateTimer();
+	}
+	
+	
+	
 
 }
