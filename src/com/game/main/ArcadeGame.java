4434c24680bdf09df07df659c4ec36bbe915bba4
David Mayer <djmayer101@gmail.com>
2012-06-19 22:34:44 -0700

    fixed timer stuff and added health to units on instantiation

--- src/com/game/main/ArcadeGame.java
+++ src/com/game/main/ArcadeGame.java
@@ -11,20 +11,20 @@ import android.widget.LinearLayout;
 public abstract class ArcadeGame extends LinearLayout {
 
 	private Context mContext;
-	
+
 	private Timer mUpdateTimer;
 	protected boolean ingame = false;
-	
+
 	public ArcadeGame(Context context) {
 		super(context);
 		mContext = context;
 	}
-	
+
 	public ArcadeGame(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		mContext = context;
 	}
-	
+
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
 		super.onLayout(changed, l, t, r, b);
 		try {
@@ -35,37 +35,41 @@ public abstract class ArcadeGame extends LinearLayout {
 		}
 	}
 
-	
-	
-	
+
+
+
 
 	protected void startUpdateTimer() {
 		//this.ingame = true;
-		stopUpdateTimer();
-		mUpdateTimer = new Timer();
+		//stopUpdateTimer();
+		if (mUpdateTimer == null){
+			mUpdateTimer = new Timer();
+		}
 		mUpdateTimer.schedule(new UpdateTask(), 0);
 	}
-	
+
 	protected void stopUpdateTimer() {
 
-		if (mUpdateTimer != null)
+		if (mUpdateTimer != null){
 			mUpdateTimer.cancel();
+			mUpdateTimer = null;
+		}
 	}
-	
-	
+
+
 	abstract protected void updatePhysics();
-	
+
 	abstract protected void initialize();
-	
+
 	abstract protected boolean isGameOver();
-	
-	
+
+
 	private class UpdateTask extends TimerTask {
 		@Override
 		public void run() {
 			long start_time = System.currentTimeMillis();
 			updatePhysics();
-			
+
 			postInvalidate();
 			long end_time = System.currentTimeMillis();
 			long remaining_time = Constants.UPDATE_CYCLE_TIME -end_time+start_time;
@@ -84,33 +88,33 @@ public abstract class ArcadeGame extends LinearLayout {
 				e.printStackTrace();
 			}
 			startUpdateTimer();
-			
+
 		}
 	}
-	
+
 	public void pause() {
 		this.ingame = false;
 		//stopUpdateTimer();
 	}
-	
+
 	public void halt() {
 		this.ingame = false;
 		stopUpdateTimer();
 	}
-	
+
 	public void resume() {
 		this.ingame = false;
 		initialize();
 		startUpdateTimer();
 	}
-	
+
 	public void go() {
 		this.ingame = true;
 		initialize();
 		//startUpdateTimer();
 	}
-	
-	
-	
+
+
+
 
 }
